a61ad977ec28a013786764e2b47b1a04197e1f56
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-11-10 12:41:47 +0500

    Task7

--- Tyuiu.MedvedevKA.Sprint4.Task7.V17.Lib/DataService.cs
+++ Tyuiu.MedvedevKA.Sprint4.Task7.V17.Lib/DataService.cs
@@ -13,8 +13,7 @@ namespace Tyuiu.MedvedevKA.Sprint4.Task7.V17.Lib
         public int Calculate(int n, int m, string value)
         {
             int[,] mtrx = new int[n, m];
-            int index = 0;
-
+         
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
